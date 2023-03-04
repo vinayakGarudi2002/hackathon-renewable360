40783e33b870708aa841dc7c3c53fdf7fb26fa14
Vivek Dhanade <vivek01110110@gmail.com>
2023-03-04 09:53:26 +0530

    modified:   src/components/Addproduct.js

--- src/components/Addproduct.js
+++ src/components/Addproduct.js
@@ -1,63 +1,28 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState ,useContext,useEffect} from "react";
 import { useNavigate } from "react-router-dom";
-import { alertContext } from "../context/Alert/AlertContext"; //----alert
-import { bgContext } from "../context/Bg";
+import { alertContext } from "../context/Alert/AlertContext";  //----alert
+import { bgContext } from '../context/Bg';
 import vendorcontext from "../context/my_vendor/vendorcontext";
-import axios from 'axios';
-import "./Style.css";
-
-
+import "./Style.css"
 const Addproduct = () => {
+const {toSetAlerts}=useContext(alertContext);   // ----alert
+const {bgColor}=useContext(bgContext);
+const {putData}=useContext(vendorcontext);
 
-  const addData = async (addingData) => {
-    const {type_of_source, service_location, subsidy_scheme, subsidy_percentage, industry_certification_name, industry_certification_no,cost_per_unit_of_clean_energy_setup,product_photo} = addingData;
 
-    try {
-      const response = await fetch(`${host}/api/vendor_data/product_data`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          mode: "cors",
-          "auth-token":
-            localStorage.getItem('token'),
-        },
-
-        body: JSON.stringify( {  type_of_source, service_location, subsidy_scheme, subsidy_percentage, industry_certification_name, industry_certification_no, cost_per_unit_of_clean_energy_setup, product_photo
-        }),
-      });
-      const data = await response.json();
-      if (response.status === 200) {
-     
-        return true;
-      } else {
-        console.log("false")
-
-        return false;
-      }
-    } catch (err) {
-      console.log("err")
-
-      console.error(err);
-
-      return false;
-    }
-  };
-  const { toSetAlerts } = useContext(alertContext); // ----alert
-  const { bgColor } = useContext(bgContext);
-  // const { addData } = useContext(vendorcontext);
 
   let navigate = useNavigate();
-  const host = "http://localhost:5000";
+  const host = "http://localhost:5000"
   // const [ typeUser , setTypeUser]=useState("")
   const [ProductForm, setProductForm] = useState({
-    type_of_source: "",
-    service_location: "",
-    subsidy_scheme: "",
-    subsidy_percentage: "0",
-    industry_certification_name: "",
-    industry_certification_no: "",
-    cost_per_unit_of_clean_energy_setup: "",
-    product_photo: ""
+    type_of_source:"",
+    service_location:"",
+    subsidy_scheme:"",
+    subsidy_percentage:"0",
+    industry_certification_name:"",
+    industry_certification_no:"",
+    cost_per_unit_of_clean_energy_setup:""
+   
   });
 
   // const [password,setPassword]=useState("");
@@ -67,79 +32,44 @@ const Addproduct = () => {
       return {
         ...prevState,
         [e.target.name]: [e.target.value].toString(),
-        
+       
       };
-
-    }
-    
-    
-    );
-    // setProductForm({...ProductForm, [e.target.name]: e.target.value});
+    });
   };
-  const handlePhoto = (e) => {
-    setProductForm({...ProductForm, product_photo: e.target.files[0]});
-}
 
-
-  const handleSubmit = async (e) => {
+  const handleSubmit=async (e)=>{
     e.preventDefault();
-    //photo
-    const formData = new FormData();
-    formData.append('product_photo', ProductForm.product_photo);
-     //photo
-    axios.post('http://localhost:5000/api/vendor_data/product_data', formData)
-         .then(res => {
-            console.log(res);
-         })
-         .catch(err => {
-            console.log(err);
-         });
-
-   //photo
-    const status = await addData(ProductForm);
-
-    if (status) {
-      navigate(`/`);
-
-      console.log(status);
-      toSetAlerts("visible", "Data Added Success Fully", "success"); //...alert
-    } else {
-      toSetAlerts("visible", "Some Error Ocurred Try Later", "danger"); //...alert
+    const status= await putData(ProductForm);
+   
+    
+    if(status){
+      navigate(`/myproducts`);
+     
+      console.log(status)
+      toSetAlerts("visible","Data Added Success Fully","success")    //...alert
+    }else{
+      toSetAlerts("visible","Some Error Ocurred Try Later","danger")    //...alert
     }
-  };
+    
+    }
+ 
+
 
   return (
-    <div className={"container signUp-container  "}>
-      <h1
-        className={"text-center my-2 login_signupForm"}
-        style={{ textShadow: `${bgColor.formText}` }}
-      >
-        Add Product
-      </h1>
-      <div
-        className="form-signUp my-2"
-        style={{
-          backgroundImage: `${bgColor.formBack}`,
-          boxShadow: `${bgColor.formBox}`,
-        }}
-      >
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          <div className="mb-1">
-            <label htmlFor="type_of_organisation" className="form-label">
-              Type of Source
-            </label>
-            <select
-              class="form-select"
-              aria-label="Default select example"
-              name="type_of_source"
-              onChange={(e) => {
-                ProductFormChange(e);
-              }}
-            >
+    <div className={"container signUp-container  "} >
+    <h1 className={"text-center my-2 login_signupForm"} style={{textShadow:`${bgColor.formText}`}}>Add Product</h1>
+     <div className="form-signUp my-2" style={{backgroundImage:`${bgColor.formBack}`,
+     boxShadow:`${bgColor.formBox}`}}>
+     <form onSubmit={(e)=>{
+            handleSubmit(e)
+        }}>
+    <div className="mb-1">
+    <label htmlFor="type_of_organisation" className="form-label" >
+    Type of Source
+          </label>
+          <select class="form-select" aria-label="Default select example"  name="type_of_source" onChange={(e) => {
+              ProductFormChange(e);
+            }} >
               <option selected>Select Type of Source</option>
               <option value="Home">Solar</option>
               <option value="Shop">Bio</option>
@@ -147,126 +77,116 @@ const Addproduct = () => {
               <option value="Shop">Solar Polycrystalline</option>
               <option value="Shop">Solar Thinfilm</option>
             </select>
-          </div>
+    </div>
 
-          <div className="mb-1">
-            <label htmlFor="location" className="form-label">
-              Service Location
-            </label>
-            <select
-              class="form-select"
-              aria-label="Default select example"
-              name="service_location"
-              onChange={(e) => {
-                ProductFormChange(e);
-              }}
-            >
-              <option selected>Select Your Location</option>
-              <option value="Mumbai">Latur</option>
-              <option value="Kolkata2">Aurangaba</option>
-              <option value="Delhi">Nashik</option>y
-            </select>
-          </div>
-          <div className="mb-1">
-            <label htmlFor="location" className="form-label">
-              Subcidy Scheme
-            </label>
-            <input
-              required
-              type="text"
-              className="form-control"
-              name="subsidy_scheme"
-              id="subsidy_scheme"
-              aria-describedby="emailHelp"
-              placeholder=""
-              value={ProductForm.subsidy_scheme}
-              onChange={(e) => {
-                ProductFormChange(e);
-              }}
-            />
-          </div>
-          <div className="mb-1">
-            <label htmlFor="location" className="form-label">
-              Subcidy Percentage
-            </label>
-            <input
-              required
-              type="text"
-              className="form-control"
-              name="subsidy_percentage"
-              id="subsidy_percentage"
-              aria-describedby="emailHelp"
-              placeholder=""
-              value={ProductForm.subsidy_percentage}
-              onChange={(e) => {
-                ProductFormChange(e);
-              }}
-            />
-          </div>
-          <div className="mb-1">
-            <label htmlFor="location" className="form-label">
-              Industries Certificate Name
-            </label>
-            <input
-              required
-              type="text"
-              className="form-control"
-              name="industry_certification_name"
-              id="industry_certification_name"
-              aria-describedby="emailHelp"
-              placeholder=""
-              value={ProductForm.industry_certification_name}
-              onChange={(e) => {
-                ProductFormChange(e);
-              }}
-            />
-          </div>
-
-          <div className="mb-1">
-            <label htmlFor="location" className="form-label">
-              Industries Certificate Number
-            </label>
-            <input
-              required
-              type="text"
-              className="form-control"
-              name="industry_certification_no"
-              id="industry_certification_no"
-              aria-describedby="emailHelp"
-              placeholder=""
-              value={ProductForm.industry_certification_no}
-              onChange={(e) => {
-                ProductFormChange(e);
-              }}
-            />
-          </div>
-          <div className="mb-1">
-            <label htmlFor="location" className="form-label">
-              Cost Per Unit of Clean Energy Setup
-            </label>
-            <input
-              required
-              type="text"
-              className="form-control"
-              name="cost_per_unit_of_clean_energy_setup"
-              id="cost_per_unit_of_clean_energy_setup"
-              aria-describedby="emailHelp"
-              placeholder=""
-              value={ProductForm.cost_per_unit_of_clean_energy_setup}
-              onChange={(e) => {
-                ProductFormChange(e);
-              }}
-            />
-          </div>
-        <div>
-        <input 
-                type="file" 
-                accept=".png, .jpg, .jpeg"
-                name="product_photo"
-                onChange={handlePhoto}
-            />
+    <div className="mb-1">
+    <label htmlFor="location" className="form-label">
+           Service Location
+          </label>
+          <select class="form-select" aria-label="Default select example"  name="service_location" onChange={(e) => {
+              ProductFormChange(e);
+            }} >
+  <option selected>Select Your Location</option>
+  <option value="Mumbai">Latur</option>
+  <option value="Kolkata2">Aurangaba</option>
+  <option value="Delhi">Nashik</option>y
+</select>
+    </div>
+    <div className="mb-1">
+          <label htmlFor="location" className="form-label">
+          Subcidy Scheme
+          </label>
+          <input
+          required
+            type="text"
+            className="form-control"
+            name="subsidy_scheme"
+            id="subsidy_scheme"
+            aria-describedby="emailHelp"
+            placeholder=""
+            value={ProductForm.subsidy_scheme}
+            onChange={(e) => {
+              ProductFormChange(e);
+            }}
+          />
         </div>
-          {/* <div className="mb-1 form-check">
+        <div className="mb-1">
+          <label htmlFor="location" className="form-label">
+          Subcidy Percentage
+          </label>
+          <input
+          required
+            type="text"
+            className="form-control"
+            name="subsidy_percentage"
+            id="subsidy_percentage"
+            aria-describedby="emailHelp"
+            placeholder=""
+            value={ProductForm.subsidy_percentage}
+            onChange={(e) => {
+              ProductFormChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-1">
+          <label htmlFor="location" className="form-label">
+          Industries Certificate Name
+          </label>
+          <input
+          required
+            type="text"
+            className="form-control"
+            name="industry_certification_name"
+            id="industry_certification_name"
+            aria-describedby="emailHelp"
+            placeholder=""
+            value={ProductForm.industry_certification_name}
+            onChange={(e) => {
+              ProductFormChange(e);
+            }}
+          />
+        </div>
+       
+        <div className="mb-1">
+          <label htmlFor="location" className="form-label">
+          Industries Certificate Number
+          </label>
+          <input
+          required
+            type="text"
+            className="form-control"
+            name="industry_certification_no"
+            id="industry_certification_no"
+            aria-describedby="emailHelp"
+            placeholder=""
+            value={ProductForm.industry_certification_no}
+            onChange={(e) => {
+              ProductFormChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-1">
+          <label htmlFor="location" className="form-label">
+          Cost Per Unit of Clean Energy Setup
+          </label>
+          <input
+          required
+            type="text"
+            className="form-control"
+            name="cost_per_unit_of_clean_energy_setup"
+            id="cost_per_unit_of_clean_energy_setup"
+            aria-describedby="emailHelp"
+            placeholder=""
+            value={ProductForm.cost_per_unit_of_clean_energy_setup}
+            onChange={(e) => {
+              ProductFormChange(e);
+            }}
+          />
+        </div>
+  
+
+       
+        {/* <div className="mb-1 form-check">
           <input
             type="checkbox"
             className="form-check-input"
@@ -276,11 +196,11 @@ const Addproduct = () => {
             Check me out
           </label>
         </div> */}
-          <button type="submit" className="btn form-btn">
-            Submit
-          </button>
-        </form>
-      </div>
+        <button type="submit" className="btn form-btn" >
+          Submit
+        </button>
+      </form>
+     </div>
     </div>
   );
 };
