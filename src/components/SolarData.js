a344a86b5d0a6cae0a684bbba865196f5d481afb
Vivek Dhanade <vivek01110110@gmail.com>
2023-03-04 11:33:21 +0530

    modified:   src/components/SolarData.js

--- src/components/SolarData.js
+++ src/components/SolarData.js
@@ -2,63 +2,69 @@ const Menu = [
     {
       id: 1,
       image: "images/imgs1.jpg",
-      name: "imgs1",
-     
-      price: "₹ 37/ Watt ",
-      description:
-        "Type of Solar Panel: Poly Crystalline Solar Panel Type: Polycrystalline Wattage (Wp): 50 Operating(Nominal) Voltage: 12VNumber of cells: 36 cells Maximum Power: 11 - 99 ",
-      Industry_Certification:"ISO Certified",
-      Subscidy_Available:"20%",
-      Contact_Details:"Mobile : 022 546 342 Email:amrutsp@gmail.com"
+      name: "Amrut Energy Pvt. Ltd.",
+      type_of_source:  "Solar",
+      location: "Aurangabad",
+      industry_certification:"ISO Certified",
+      industry_certification_no: 123456,
+      subsidy_scheme:"ABC Scheme",
+      subsidy_percentage:"20%",
+      cost_per_unit: "₹ 37/ Watt ",
+      contact_details:"Mobile : 022 546 342 Email:amrutsp@gmail.com"
     },
   
     {
       id: 2,
-      image: "images/imgs2.png",
-      name: "imgs2",
-      category: "",
-      price: "₹ 28/ Watt ",
-      description:
-        "Brand	BIRKAN SOLAR Solar Panel Type	Monocrystalline Wattage (Wp)	400 Operating(Nominal) Voltage	24V Number of cells	72 cells",
-        Industry_Certification:"IAF Certified",
-        Subscidy_Available:"40%",
-        Contact_Details:"Mobile : 022 784 312 Email:gsp@gmail.com"
+      image: "images/imgs2.jpg",
+      name: "Havells Energy Pvt. Ltd.",
+      type_of_source:  "Solar",
+      location: "Nashik",
+      industry_certification:"ISO Certified",
+      industry_certification_no: 123456,
+      subsidy_scheme:"ABC Scheme",
+      subsidy_percentage:"20%",
+      cost_per_unit: "₹ 35/ Watt ",
+      contact_details:"Mobile : 022 546 342 Email:havells@gmail.com"
     },
     {
       id: 3,
-      image: "images/imgsp3.png",
-      name: "imgsp3",
-      category: "",
-      price: "₹ 25/ Watt",
-      description:
-        "Operating Voltage	24 V Dimensions	300w onwards Warranty	Upto 1 Year* Product Type	Poly crystalline Type of Solar Power Panel	Poly Crystalline",  
-        Industry_Certification:"BSI Certified",
-        Subscidy_Available:"30%",
-        Contact_Details:"Mobile : 022 987 305 Email:solartech@gmail.com"
+      image: "images/imgs3.jpg",
+      name: "Emmvee Group",
+      type_of_source:  "Solar",
+      location: "Dhule",
+      industry_certification:"ISO Certified",
+      industry_certification_no: 123456,
+      subsidy_scheme:"ABC Scheme",
+      subsidy_percentage:"20%",
+      cost_per_unit: "₹ 37/ Watt ",
+      contact_details:"Mobile : 022 546 342 Email:emvee@gmail.com"
     },
     
     {
       id: 4,
-      image: "../images/imgs4.png",
-      name: "imgs4",
-      price: "₹ 45/ Watt  ",
-      description:
-        "Solar Panel Type	Polycrystalline Number of cells	72 cells Warranty	25 Years Voltage	12 V Wattage	330 W" ,
-        Industry_Certification:"ISO Certified",
-        Subscidy_Available:"10%",
-        Contact_Details:"Mobile : 022 458 156 Email:renewtech@gmail.com"
+      image: "images/imgs4.jpg",
+      name: "Azure Power",
+      type_of_source:  "Solar",
+      location: "Panvel",
+      industry_certification:"ISO Certified",
+      industry_certification_no: 123456,
+      subsidy_scheme:"ABC Scheme",
+      subsidy_percentage:"20%",
+      cost_per_unit: "₹ 38/ Watt ",
+      contact_details:"Mobile : 022 546 342 Email:azurepower@gmail.com"
     },
     {
       id: 5,
-      image: "../images/imgs5.png",
-      name: "imgs5",
-      
-      price: "₹ 23,999/ Piece  ",
-      description:
-      "Brand	IFITech Solar Panel Type	Monocrystalline Operating(Nominal) Voltage	5V"    ,
-      Industry_Certification:"IAF Certified",
-      Subscidy_Available:"25%",
-      Contact_Details:"Mobile : 022 341 157 Email:rstech@gmail.com"
+      image: "images/imgs5.jpg",
+      name: "Vikram Solar",
+      type_of_source:  "Solar",
+      location: "Latur",
+      industry_certification:"ISO Certified",
+      industry_certification_no: 123456,
+      subsidy_scheme:"ABC Scheme",
+      subsidy_percentage:"20%",
+      cost_per_unit: "₹ 32/ Watt ",
+      contact_details:"Mobile : 022 546 342 Email:vikramsolarp@gmail.com"
 
     }
     
